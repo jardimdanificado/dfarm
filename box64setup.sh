5c1eaa02a82fe93ee43b2f9f5dfd6273f1b630c2
jardimdanificado <jardeltorreslima01@gmail.com>
2022-07-16 16:40:46 -0300

    update

--- box64setup.sh
+++ box64setup.sh
@@ -2,7 +2,7 @@ sudo apt install git build-essential cmake
 git clone https://github.com/ptitSeb/box64.git
 cd box64
 mkdir build
-cmake .. -DRPI4ARM64=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo
+cmake -DRPI4ARM64=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo
 make -j4
 sudo make install
 systemctl restart systemd-binfmt
